cc106990859ba536eff5e7dd76fc01466ef34005
hiqmal13 <119303535+hiqmal13@users.noreply.github.com>
2023-12-19 18:16:51 +0700

    Add files via upload

---
+++ UAS/hiqmal_spk web/Samsung tipe S_Ponsel.sql
@@ -0,0 +1,24 @@
+-- CREATE TABLE Ponsel (
+--     Brand VARCHAR(50),
+--     RAM VARCHAR(10),
+--     Prosesor VARCHAR(50),
+--     Storage VARCHAR(20),
+--     Baterai VARCHAR(20),
+--     Harga VARCHAR(20),
+--     Ukuran_Layar VARCHAR(20)
+-- );
+
+-- INSERT INTO Ponsel (Brand, RAM, Prosesor, Storage, Baterai, Harga, Ukuran_Layar)
+-- VALUES
+--     ('Samsung Galaxy S23', '8 GB', 'SM8550 Snapdragon 8 Gen 2', '128 GB', '3.900 mAh', 'Rp.11.050.000', '6.1 inch'),
+--     ('Samsung Galaxy S23+', '8 GB', 'SM8550 Snapdragon 8 Gen 2', '256 GB', '4.700 mAh', 'Rp13.850.000', '6.6 inch'),
+--     ('Samsung Galaxy S23 Ultra 5G', '12 GB', 'SM8550-AC Snapdragon 8 Gen 2', '256 GB', '5.000 mAh', 'Rp.19.999.000', '6.8 inch'),
+--     ('Samsung Galaxy S22 5G', '8 GB', 'Snapdragon 8 Gen 1', '128 GB', '3.700 mAh', 'Rp.8.600.000', '6.1 inch'),
+--     ('Samsung Galaxy S22 Plus 5G', '8 GB', 'Snapdragon 8 Gen 1', '128 GB', '4.500 mAh', 'Rp.9.600.000', '6.6 inch'),
+--     ('Samsung Galaxy S22 Ultra 5G', '12 GB', 'Snapdragon 8 Gen 1', '256 GB', '5.000 mAh', 'Rp.14.500.000', '6.8 inch'),
+--     ('Samsung Galaxy S21 FE 5G', '8 GB', 'Dimensity 2100', '128 GB', '4.500 mAh', 'Rp.7.500.000', '6.4 inch'),
+--     ('Samsung Galaxy S21 5G', '8 GB', 'Dimensity 2100', '256 GB', '4.000 mAh', 'Rp.12.000.000', '6.2 inch'),
+--     ('Samsung Galaxy S21 + 5G', '8 GB', 'Dimensity 2100', '256 GB', '4.800 mAh', 'Rp.14.300.000', '6.7 inch'),
+--     ('Samsung Galaxy S21 Ultra 5G', '12 GB','Dimensity 2100', '256 GB', '5.000 mAh', 'Rp.8.899.000', '6.8 inch');
+	
+ select *from Ponsel;
---
+++ UAS/hiqmal_spk web/settings.py
@@ -0,0 +1,69 @@
+USER = 'postgres'
+PASSWORD = '123'
+HOST = 'localhost'
+PORT = '5432'
+DATABASE_NAME = 'UASibnu' 
+
+MEREK_SCALE = {
+    "Samsung Galaxy S23": 9,
+    "Samsung Galaxy S23+": 9,
+    "Samsung Galaxy S23 Ultra 5G": 9,
+    "Samsung Galaxy S22 5G": 8,
+    "Samsung Galaxy S22 Plus 5G": 8,
+    "Samsung Galaxy S22 Ultra 5G": 9,
+    "Samsung Galaxy S21 FE 5G": 7,
+    "Samsung Galaxy S21 5G": 8,
+    "Samsung Galaxy S21 + 5G": 8,
+    "Samsung Galaxy S21 Ultra 5G": 9
+}
+
+DEV_SCALE_ram = {
+    "8 GB": 8,
+    "12 GB": 9
+}
+
+DEV_SCALE_prosesor = {
+    "SM8550 Snapdragon 8 Gen 2": 9,
+    "SM8550-AC Snapdragon 8 Gen 2": 9,
+    "Snapdragon 8 Gen 1": 8,
+    "Dimensity 2100": 7
+}
+
+DEV_SCALE_storage = {
+    "128 GB": 8,
+    "256 GB": 9
+}
+
+DEV_SCALE_baterai = {
+    "3.900 mAh": 7,
+    "4.700 mAh": 8,
+    "5.000 mAh": 9,
+    "3.700 mAh": 8,
+    "4.500 mAh": 8,
+    "4.000 mAh": 8,
+    "4.800 mAh": 8
+}
+
+DEV_SCALE_harga = {
+    "Rp.11.050.000": 9,
+    "Rp13.850.000": 9,
+    "Rp.19.999.000": 9,
+    "Rp.8.600.000": 8,
+    "Rp.9.600.000": 8,
+    "Rp.14.500.000": 9,
+    "Rp.7.500.000": 7,
+    "Rp.12.000.000": 8,
+    "Rp.14.300.000": 8,
+    "Rp.8.899.000": 9
+}
+
+DEV_SCALE_ukuran_layar = {
+    "6.1 inch": 7,
+    "6.6 inch": 8,
+    "6.8 inch": 9,
+    "6.4 inch": 7,
+    "6.2 inch": 8,
+    "6.7 inch": 8
+}
+
+
