8466de7ff1ac494addb06c24e5aa323769535298
hiqmal13 <119303535+hiqmal13@users.noreply.github.com>
2023-12-19 18:32:29 +0700

    Delete UAS/hiqmal_spk model directory

--- UAS/hiqmal_spk model/engine.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from settings import USER, PASSWORD, HOST, PORT, DATABASE_NAME
-
-# Membuat koneksi menggunakan SQLAlchemy
-engine = create_engine(
-    f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE_NAME}"
-)
